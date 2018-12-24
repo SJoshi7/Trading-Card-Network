a9fec7845f442985377700e3875b89e098425dce
SJOSHI7 <sjoshise7@gmail.com>
2018-12-24 01:19:53 -0500

    logic file

--- cards-trading-network/lib/logic.js
+++ cards-trading-network/lib/logic.js
@@ -1,25 +1,23 @@
-import { emit } from "cluster";
-
-/**
+/*
  * Buy card transaction
  * @param {org.acme.tradenet} trade
  * @transaction
  */
-
- async function {
-     if(trade.card.forTrade){
-         trade.card.owner = trade.newOwner;
-         return getAssetRegistry("org.acme.tradenet")
-            .then(assetRegistry ==> {
-                return assetRegistry.update(trade.card);
-            })
-            .then(()=>{
-                let event = getFactory.newEvent(
-                    "org.acme.tradenet"
-                    "TradeNotification"
-                );
-                event.card = trade.card;
-                emit(event);
-            })
-     }
- }
+async function buyCard(trade) {
+    if (trade.card.forTrade) {
+      // If card is available for trade
+      trade.card.owner = trade.newOwner;
+      return getAssetRegistry("org.acme.tradenet")
+        .then(assetRegistry => {
+          return assetRegistry.update(trade.card); // Update the network registry
+        })
+        .then(() => {
+          let event = getFactory().newEvent(
+            "org.acme.",
+            "TradeNotification"
+          ); // Get a reference to the event specified in the modeling language
+          event.card = trade.card;
+          emit(event); // Fire off the event
+        });
+    }
+  }
